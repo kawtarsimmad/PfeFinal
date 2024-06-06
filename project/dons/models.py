22ec92e14839c77478a1e8675353db3657dfc171
kawtarsimmad <kawtarsimmad02@gmail.com>
2024-06-06 12:27:28 +0100

    update model don

--- project/dons/models.py
+++ project/dons/models.py
@@ -3,13 +3,13 @@ from django.utils import timezone
 from users.models import Donor,User,Association
 from publications.models import Publication
 class Don(models.Model): 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     id=models.AutoField(primary_key=True)
     date=models.DateTimeField(default=timezone.now)
     montantDons=models.DecimalField(max_digits=10,decimal_places=4)
     est_paye = models.BooleanField(default=False)
-    publication = models.ForeignKey(Publication, on_delete=models.CASCADE, related_name='dons', null=True, blank=True)
-    association = models.ForeignKey(Association, on_delete=models.CASCADE, related_name='dons', null=True, blank=True)
+    publication = models.ForeignKey(Publication, on_delete=models.SET_NULL, related_name='dons', null=True, blank=True)
+    association = models.ForeignKey(Association, on_delete=models.SET_NULL, related_name='dons', null=True, blank=True)
     is_anonymous = models.BooleanField(default=False)
 
     
